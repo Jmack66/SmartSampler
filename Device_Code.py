3ccb0c283a36f92d5605affefff3c93ac273b30f
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2020-04-28 10:46:16 +0100

    Update Device_Code.py

--- Device_Code.py
+++ Device_Code.py
@@ -1,23 +1,52 @@
 import RPi.GPIO as gpio
-from time import sleep
+import time
+
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 
 gpio.setup(12,gpio.OUT)
+gpio.setup(16, gpio.IN)
+
+stepperPin = [18,22,24,26]
 gpio.setup(18,gpio.OUT)
 gpio.setup(22,gpio.OUT)
 gpio.setup(24,gpio.OUT)
 gpio.setup(26,gpio.OUT)
+
 servo = gpio.PWM(12,50)
 
 servo.start(0)
 
 gpio.output(12,True)
 
-servo.ChangeDutyCycle(90)
-gpio.output(18,1)
-sleep(1)
-servo.ChangeDutyCycle(0)
-gpio.output(12,False)
 
+def servoController(angle):
+	out = (angle / 18) + 2
+	gpio.output(12,True)
+	pwm.ChangeDutyCycle(out)
+	gpio.output(12,False)
+
+def infraredInput():
+	ir = gpio.input(16)
+	print(ir)
+
+def stepperController(rotate):
+	seq = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
+	if rotate:
+		for i in range(128):
+			for step in range(8):
+					for pin in range(4):
+						gpio.output(stepperPin[pin], seq[step][pin])
+				time.sleep(0.002)	
+
+def main():
+	servoController(0)
+	stepperController(True)
+	for i in range(100):
+		infraredInput()
+	servoController(90)
+
+
+
+main()
 gpio.cleanup()
