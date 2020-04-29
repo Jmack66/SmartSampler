82bfdfa91781bc24f2d62b2c62945e637a06d5c5
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2020-04-29 19:51:10 +0100

    Update Big_Brain.ino

--- Big_Brain/Big_Brain.ino
+++ Big_Brain/Big_Brain.ino
@@ -4,11 +4,12 @@
 #define IR_D  28
 #define STEPPER1 27
 #define STEPPER2 26
-#define STEPPER2 25
-#define STEPPER2 24
-
+#define STEPPER3 25
+#define STEPPER4 24
 
 Servo servo;
+int ir_analog;
+bool ir_digital;
 
 void setup() {
   pinMode(IR_A, INPUT);
@@ -21,6 +22,28 @@ void setup() {
 }
 
 void loop() {
-
+  // check ir 
+  // if object is present, set object state to true ,servo rotate for given time
+  // check ir
+  // if object is gone, back servo would dispense
+  // stepper motor rotates quarter rotation
 
 }
+
+void getIR(){
+  ir_analog = analogRead(IR_A);
+  ir_digital = digitalRead(IR_D);
+  }
+
+void stepperController(){
+  int seq[8][4] = {{1,0,0,0},{1,1,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0},{0,0,1,1},{0,0,0,1},{1,0,0,1}};
+  
+  }
+
+
+
+
+
+
+
+  
