9885941ca8b64866179e2b7c0b44bea9a909c461
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2020-04-30 12:11:16 +0100

    Update Big_Brain.ino

--- Big_Brain/Big_Brain.ino
+++ Big_Brain/Big_Brain.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <PWMServo.h>
 
 #define IR_A 32
 #define IR_D  28
@@ -7,10 +7,11 @@
 #define STEPPER3 25
 #define STEPPER4 24
 
-Servo servo;
+PWMServo servo;
 int ir_analog;
 bool ir_digital;
-
+int stepperPins[4] = {STEPPER1,STEPPER2,STEPPER3,STEPPER4};
+int seq[8][4] = {{1,0,0,0},{1,1,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0},{0,0,1,1},{0,0,0,1},{1,0,0,1}};
 void setup() {
   pinMode(IR_A, INPUT);
   pinMode(IR_D, INPUT);
@@ -19,6 +20,7 @@ void setup() {
   pinMode(STEPPER3, OUTPUT);
   pinMode(STEPPER4, OUTPUT);
   servo.attach(2);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -27,17 +29,25 @@ void loop() {
   // check ir
   // if object is gone, back servo would dispense
   // stepper motor rotates quarter rotation
-
+  stepperController();
+  getIR();
 }
 
 void getIR(){
   ir_analog = analogRead(IR_A);
+  Serial.println(ir_analog);
   ir_digital = digitalRead(IR_D);
   }
 
 void stepperController(){
-  int seq[8][4] = {{1,0,0,0},{1,1,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0},{0,0,1,1},{0,0,0,1},{1,0,0,1}};
-  
+  for(int i = 0; i < 128; i++){
+    for(int s = 0; s < 8; s++){
+      for(int p = 0; p < 4; p++){
+        delay(1);
+        digitalWrite(stepperPins[p],seq[s][p]);
+        }
+      }
+    }
   }
 
 
