5927811ebc8c453e1b0e6ca405fecab17d40336c
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2020-05-09 14:13:34 +0100

    Update Big_Brain.ino

--- Big_Brain/Big_Brain.ino
+++ Big_Brain/Big_Brain.ino
@@ -1,4 +1,4 @@
-#include <PWMServo.h>
+#include <Servo.h>
 
 #define IR_A 32
 #define IR_D  28
@@ -7,12 +7,11 @@
 #define STEPPER3 25
 #define STEPPER4 24
 
-PWMServo servo;
+Servo servo;
 int ir_analog;
 bool ir_digital;
 int stepperPins[4] = {STEPPER1, STEPPER2, STEPPER3, STEPPER4};
 int seq[8][4] = {{1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {1, 0, 0, 1}};
-int setpoint;
 
 void setup() {
   pinMode(IR_A, INPUT);
@@ -22,37 +21,59 @@ void setup() {
   pinMode(STEPPER3, OUTPUT);
   pinMode(STEPPER4, OUTPUT);
   servo.attach(2);
-  Serial.begin(9600);
-  setpoint = getIRA();
 }
+
+struct state{
+  bool drop = false;
+  bool rotate = false;
+  bool collect = false;
+  bool pour = false;
+  bool error = false; 
+  bool startup = false;
+  
+  }sampler;
+
 
 void loop() {
-  //rotate until an object is present, allow pourer
-  while(getIRD()){
-    stepperController();
-    }
-    servoController();
+
 }
 
-int getIRA() {
-  ir_analog = analogRead(IR_A);
-  return ir_analog;
-}
-bool getIRD() {
-  ir_digital = digitalRead(IR_D);
-  return ir_digital;
-}
-void servoController(){
-  servo.write(90);
-  delay(250);
-  servo.write(0);
-  }
 
-void stepperController() {
-    for (int s = 0; s < 8; s++) {
-      for (int p = 0; p < 4; p++) {
-        delay(1);
-        digitalWrite(stepperPins[p], seq[s][p]);
-      }
+class IR{
+  private:
+    int pin;
+  public:
+    int ir_d;
+  void setPin(p){
+    pin = pnum;
     }
-}
+  void getIR(){
+    ir_d = digitalRead(pin);
+    }
+  };
+
+
+
+
+
+
+
+
+//int getIRA() {
+//  ir_analog = analogRead(IR_A);
+//  return ir_analog;
+//}
+//bool getIRD() {
+//  ir_digital = digitalRead(IR_D);
+//  return ir_digital;
+//}
+//
+//
+//void stepperController() {
+//    for (int s = 0; s < 8; s++) {
+//      for (int p = 0; p < 4; p++) {
+//        delay(1);
+//        digitalWrite(stepperPins[p], seq[s][p]);
+//      }
+//    }
+//}
