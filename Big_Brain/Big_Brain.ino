c8316aef66892e578a487f35ec73da0c79c2385d
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2020-05-11 20:17:56 +0100

    Update Big_Brain.ino

--- Big_Brain/Big_Brain.ino
+++ Big_Brain/Big_Brain.ino
@@ -1,42 +1,23 @@
 #include <Servo.h>
 #include <Stepper.h>
-#define STEPS 513
-#define FRONT_PIN 3
-#define BACK_PIN 3
-#define SPEED 30
-#define S11 27
-#define S12 26
-#define S13 25
-#define S14 24
-#define S21 27
-#define S22 26
-#define S23 25
-#define S24 24
+#define STEPS 32
+#define FRONT_PIN 2
+#define BACK_PIN 4
+#define SPEED 640
+#define S11 9
+#define S12 10
+#define S13 11
+#define S14 12
+#define S21 5
+#define S22 6
+#define S23 7
+#define S24 8
 #define POUR_TIME 10 //time in miliseconds
 
 Servo nozzle;
 Stepper disc(STEPS, S11, S12, S13, S14);
 Stepper drop(STEPS, S21, S22, S23, S24);
 
-
-
-class Infrared {
-  private:
-    bool ir_d;
-    int pin;
-  public:
-    Infrared(int pin){
-      this->pin = pin;
-      pinMode(pin,INPUT); 
-      }
-
-    bool getIR() {
-      ir_d = digitalRead(pin);
-      return ir_d;
-    }
-};
-Infrared frontSensor = Infrared(FRONT_PIN);
-Infrared backSensor = Infrared(BACK_PIN);
 struct state {
   bool drop = false;
   bool rotate = false;
@@ -52,19 +33,34 @@ void setup() {
   disc.setSpeed(SPEED);
   drop.setSpeed(SPEED);
   Serial.begin(9600);
+  pinMode(FRONT_PIN,INPUT);
+  pinMode(BACK_PIN,INPUT);
+  sampler.startup = true;
 }
 
 void dropCup() {
-  while (backSensor.getIR()) {
-    drop.step(5);
+  while (backSensor()) {
+    Serial.println(backSensor());
+    //Serial.println(frontSensor());
+    drop.step(1);
   }
+  Serial.println("cup dropped stopping");
+  //drop.step(10);//prime the next cup
 }
 
 void rotate() {
-  while (frontSensor.getIR()) {
-    disc.step(5);
+  while (frontSensor()) {
+    Serial.println("now rotating");
+    disc.step(1);
   }
 }
+
+bool frontSensor(){
+  return digitalRead(FRONT_PIN);
+  }
+bool backSensor(){
+  return digitalRead(BACK_PIN);
+  }
 void dispense(int pour_time) {
   nozzle.write(90);
   delay(pour_time);
@@ -74,15 +70,17 @@ void dispense(int pour_time) {
 void startup(bool check) {
   if (check) {
     dropCup();
-    drop.step(50); //prime the next cup
+    disc.step(160);
+    Serial.println("priming next cup");
+    delay(5000);
+    dropCup();
     rotate();
     Serial.println("Startup complete");
     sampler.startup = false;
   }
 }
 
-
-
 void loop() {
-
+  startup(sampler.startup);
+  
 }
